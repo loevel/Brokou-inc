32848c0fde23daa12046e7b1b21f68451c19bddc
Dave Loevel Chendjou Sihanou <davesihanou@gmail.com>
2025-10-02 01:01:42 +0000

    configure ceci dans le midleware: {   "route": "/login",   "redirect": "

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,21 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('brokou_auth_token');
 
-  if (!token) {
-    const loginUrl = new URL('/login', request.url);
-    loginUrl.searchParams.set('from', request.nextUrl.pathname);
-    return NextResponse.redirect(loginUrl);
+  if (request.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/.auth/login/aad', request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith('/admin')) {
+    if (!token) {
+      const loginUrl = new URL('/login', request.url);
+      loginUrl.searchParams.set('from', request.nextUrl.pathname);
+      return NextResponse.redirect(loginUrl);
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/admin/:path*',
+  matcher: ['/admin/:path*', '/login'],
 }
